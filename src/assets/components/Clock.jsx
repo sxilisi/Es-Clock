8a1166f68f12191aff1e6cf3ecea5da0a75dc1b2
sxilisi <raffaelli.simone98@gmail.com>
2024-05-31 14:52:44 +0200

    add clearIterval

--- src/assets/components/Clock.jsx
+++ src/assets/components/Clock.jsx
@@ -4,9 +4,13 @@ export default function Clock() {
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       setDate(new Date());
     }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
   return (
     <div>
